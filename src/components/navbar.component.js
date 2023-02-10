573296cd92a1f13bb60113e911396c61694fd13b
edwin-737 <76090138+edwin-737@users.noreply.github.com>
2023-02-10 14:20:19 +0800

    show graph data

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -36,7 +36,7 @@ export default function Navbar() {
 
         }
         getTeamsToken();
-    }, [graphData]);
+    }, [graphData, retrieved]);
     return (
         <div>
             <div className="nav">
